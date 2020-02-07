4e0a9b85673ee48766a347154c800951bb8ac3b2
Brian Hague <alphaseinor@gmail.com>
2020-02-06 21:16:10 -0600

    boilerplate for helper methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/dbconfig')
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+}
+
+function find(){
+
+}
+
+function findById(){
+
+}
+
+function findSteps(){
+
+}
+
+function add(){
+
+}
+
+function update(){
+
+}
+
+function remove(){
+  
+}
